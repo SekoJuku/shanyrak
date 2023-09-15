bc7521db14a93872a18c568f27cb9530ec7c9d25
SekoJuku <serikzhan.kuanishev@kaspi.kz>
2023-09-15 14:30:26 +0600

    init

---
+++ src/main/java/com/github/shanyrak/ShanyrakApplication.java
@@ -0,0 +1,13 @@
+package com.github.shanyrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShanyrakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShanyrakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/shanyrak/ShanyrakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.shanyrak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShanyrakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
